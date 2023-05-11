302aa493e5966739ba22e5cbfe45e8e200ea9898
Thant Wai Soe <thantwaisoe456@gmail.com>
2023-05-11 13:44:43 +0630

    added question and dynamic rendering (single question)

---
+++ src/components/Question.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="question-container">
+        <h1 class="question">{{ question.text }}?</h1>
+    </div>
+    <div v-for="option in question.options" :key="option.id" class="option-container">
+        <div class="option">
+            <p class="option-label">{{ option.label }}</p>
+            <p class="option-value">{{ option.text }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const data = defineProps(['currentQuestion'])
+const {currentQuestion: question} = data
+
+</script>
+
+<style scoped></style>
